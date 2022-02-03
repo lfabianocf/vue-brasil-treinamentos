3e5fbf18478449e28aaba92e9382906c824e0c6d
lfabianocf <lfabianocf@gmail.com>
2022-02-03 17:48:45 -0300

    hearder

--- src/components/HeaderLogged/index.vue
+++ src/components/HeaderLogged/index.vue
@@ -1,37 +1,39 @@
 <template>
   <div class="flex items-center justify-between w-4/5 max-w-6xl py-10">
-    <div class="w-28 lg:w-36 ">
-      <img  class="w-full" src="../../assets/images/logo_white.png" alt="logo">
+    <div class="w-28 lg:w-36">
+      <img class="w-full" src="../../assets/images/logo_white.png" alt="logo">
     </div>
 
-    <div class="flex"  >
+    <div class="flex">
       <ul class="flex list-none">
         <li
-        @click="() => router.push({ name: 'Credencials'})"
-        class="px-6 py-2 font-bold text-white rounded-full cursor-pointer focus:ouline-none">
-        Credenciais</li>
-
-        <li
-        @click="() => router.push({ name: 'Feedbacks'})"
-        class="px-6 py-2 font-bold text-white rounded-full cursor-pointer focus:ouline-none">
-        Feedbacks
+          @click="() => router.push({ name: 'Credencials' })"
+          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
+        >
+          Credenciais
         </li>
         <li
-        @click="handleLogout"
-        class="px-6 py-2 font-bold bg-white rounded-full cursor-pointer focus:ouline-none"
-         >
-        {{ logoutLabel }}
+          @click="() => router.push({ name: 'Feedbacks' })"
+          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
+        >
+          Feedbacks
+        </li>
+        <li
+          id="logout-button"
+          @click="handleLogout"
+          class="px-6 py-2 font-bold bg-white rounded-full cursor-pointer text-brand-main focus:outline-none"
+        >
+          {{ logoutLabel }}
         </li>
       </ul>
     </div>
-
   </div>
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core'
 import { useRouter } from 'vue-router'
-import useStore from '../../hooks/userStore'
+import { computed } from 'vue'
+import useStore from '../../hooks/useStore'
 import { cleanCurrentUser } from '../../store/user'
 
 export default {
@@ -58,6 +60,5 @@ export default {
       handleLogout
     }
   }
-
 }
 </script>
