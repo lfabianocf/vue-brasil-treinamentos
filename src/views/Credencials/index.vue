a0bc4201a1934f0977d6a05362ec6d8c5765992c
lfabianocf <lfabianocf@gmail.com>
2022-02-04 18:13:40 -0300

    credencia parte 2

--- src/views/Credencials/index.vue
+++ src/views/Credencials/index.vue
@@ -7,16 +7,71 @@
   <h1 class="text-4xl font-black text-center text-gray-800">
     Credenciais
   </h1>
-  <p class="text-lg text-center text-gray-800  font-regular">
+  <p class="text-lg text-gray-800  font-regular">
     Guia de instalação e geração de suas Credenciais
   </p>
+</div>
+
+<div class="flex justify-center w-full h-full">
+  <div class="flex flex-col w-4/5 max-w-6lx py-10 ">
+    <h1 class="text-3xl font-black text-brand-darkgray">
+    Instalação e Configuração
+    </h1>
+    <p class="mt-10 text-lg text-gray-800 font-regular">
+    Este aqui é a sua chave de api
+    </p>
+    <div
+      class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2"
+    >
+      <span>{{ store.User.currentUser.apikey }}</span>
+        <div class="flex ml-20 mr-5">
+          <icon
+            name="copy"
+            :color="brandColors.graydark"
+            size="24"
+            class="cursor-pointer"
+          />
+
+            <icon
+            name="loading"
+            :color="brandColors.graydark"
+            size="24"
+            class="cursor-pointer ml-3"
+          />
+        </div>
+    </div>
+
+    <p class="mt-5 text-lg text-gray-800 font-regular">
+    Coloque o script abaixo no seu site para começar a receber feedbacks
+    </p>
+
+    <div
+      class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-2/3 overflow-x-scroll"
+    >
+      <pre>
+ &lt;script src=""https://lfabianocf-feedbacks-widget.netlify.app?api_key={{store.User.currentUser}}"
+      &gt;&lt;/script&gt;
+      </pre>
+    </div>
+
+  </div>
 </div>
 </template>
 
 <script>
 import HeaderLogged from '../../components/HeaderLogged'
+import Icon from '../../components/Icon'
+import useStore from '../../hooks/useStore'
+import palette from '../../../palette'
 
 export default {
-  components: { HeaderLogged }
+  components: { HeaderLogged, Icon },
+  setup () {
+    const store = useStore()
+    return {
+      store,
+      brandColors: palette.brand
+    }
+  }
 }
 </script>
