18679f8d41a7611d3131aebecd1ee638824b2e31
Fabiano Fernandes <lfabianocf@gmail.com>
2022-02-22 20:57:51 -0300

    erro linha 19: :class="`bg-${filter.color}`"

--- src/views/Feedbacks/filters.vue
+++ src/views/Feedbacks/filters.vue
@@ -1,31 +1,30 @@
 <template>
  <div class="flex flex-col">
-  <h1 class="text-2xl font-regular text-brand-darkgray">
-    Filtros
+   <h1 class="text-2xl font-regular text-brand-darkgray">
+      Filtros
    </h1>
+
    <ul class="flex flex-col mt-3 list-none">
     <li
-    v-for="filter in state.filters"
-    :key="filter.label"
-    :class="{
-    'bg-gray-200 bg-opacity-50': filter.active
-    }"
-    @click="() => handleSelect(filter)"
-    class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
+      v-for="filter in state.filters"
+      :key="filter.label"
+      :class="{
+      'bg-gray-200 bg-opacity-50': filter.active
+      }"
+      @click="() => handleSelect(filter)"
+      class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
     >
-        <div class="flex items-center">
-        <span
-        :class="`bg-$filter.color`"
-        class="inline-block w-2 h-2 mr-2 rounded-full">
-        {{ filter.label }}
-        </span>
-        </div>
-        <span
-        :class="filter.active ? `text-${filter.COLORS}` : 'text-brand-gray' "
-        class="font-bold"
-        >
+      <div class="flex items-center">
+       <span
+        :class="`bg-${filter.color}`"
+        class="inline-block w-2 h-2 mr-2 rounded-full"> </span> {{ filter.label}}
+      </div>
+      <span
+        :class="filter.active ? `text-${filter.color}` : 'text-brand-graudark'"
+        class="font-regular"
+      >
         {{ filter.amount }}
-        </span>
+      </span>
     </li>
    </ul>
   </div>
@@ -91,7 +90,7 @@ export default {
         return
       }
       state.filters = state.filters.map((filter) => {
-        if (filter.tyoe === type) {
+        if (filter.type === type) {
           return { ...filter, active: true }
         }
         return { ...filter, active: false }
