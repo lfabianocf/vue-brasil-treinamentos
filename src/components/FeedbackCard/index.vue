1ac713e4fee5e04d668474dd51a6aa66d9e5f115
Fabiano Fernandes <lfabianocf@gmail.com>
2022-02-23 20:26:53 -0300

    feedbacks parte 5

--- src/components/FeedbackCard/index.vue
+++ src/components/FeedbackCard/index.vue
@@ -6,26 +6,78 @@
       <badge :type="feedback.type" />
 
       <span class="font-regular text-brand-graydark">
-        {{ feedback.createdAt }}
+       {{ getDiffTimeBetweenCurrentDate(feedback.createdAt) }}
       </span>
       </div>
+
+      <div class="text-lg font-medium text-gray-800 ">
+       {{ feedback.text }}
+      </div>
+
+      <div
+        :class="{
+          animate__fadeOutUp: state.isClosing
+        }"
+        class="flex mt-8 animate__animated animate__fadeInUp animate__faster"
+        v-if="state.isOpen"
+      >
+        <div class="flex flex-col w-1/2">
+            <div class="flex flex-col">
+              <span class="font-bold text-gray-400 uppercase select-none">Página</span>
+              <span class="font-bold text-gray-700 uppercase select-none">{{ feedback.page }}</span>
+            </div>
+            <div class="flex flex-col">
+              <span class="font-bold text-gray-400 uppercase select-none">Dispositivo</span>
+              <span class="font-bold text-gray-700 uppercase select-none">{{ feedback.device }}</span>
+            </div>
+        </div>
+
+        <div class="flex flex-col w-1/2">
+            <div class="flex flex-col">
+              <span class="font-bold text-gray-400 uppercase select-none">Usuário</span>
+              <span class="font-bold text-gray-700 uppercase select-none">{{ feedback.fingerprint }}</span>
+            </div>
+        </div>
+
+        <div class="flex justify-end mt-8" v-if="!state.isOpen">
+          <icon name="chevron-down" size="24" :color="brandColors.graydark"/>
+        </div>
+    </div>
 
   </div>
 </template>
 
 <script>
+import Icon from '../Icon'
 import Badge from './Badge'
+import { getDiffTimeBetweenCurrentDate } from '../../utils/date'
+import palette from '../../../palette'
+import { reactive } from '@vue/reactivity'
+import { wait } from '../../utils/timeout'
+
 export default {
-  components: { Badge },
+  components: { Badge, Icon },
   props: {
+    isOpened: { type: Boolean, default: false },
     feedback: { type: Object, required: true }
   },
-  setup () {
-    function handleToggle () {
-
+  setup (props) {
+    const state = reactive({
+      isOpen: props.isOpened,
+      isClosing: !props.isOpened
+    })
+    async function handleToggle () {
+      state.isClosing = true
+      await wait(250)
+      state.isOpen = !state.isOpen
+      state.isClosing = false
     }
+
     return {
-      handleToggle
+      state,
+      handleToggle,
+      getDiffTimeBetweenCurrentDate,
+      brandColors: palette.brand
     }
   }
 }
